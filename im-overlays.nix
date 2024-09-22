1850356c97c96c8d127ceb76e291aa6626673176
lachrymaLF <lachrymalfutura@gmail.com>
2024-09-22 00:42:03 -0400

    d

--- im-overlays.nix
+++ im-overlays.nix
@@ -1,10 +1,9 @@
+# this sucks
 final: prev: {
   vesktop = prev.vesktop.overrideAttrs (oldAttrs: {
-    postFixup = builtins.replaceStrings [ "app.asar" ] [ "app.asar --set GTK_IM_MODULE fcitx" ] oldAttrs.postFixup; # this sucks
+    postFixup = builtins.replaceStrings [ "app.asar" ] [ "app.asar --set GTK_IM_MODULE fcitx" ] oldAttrs.postFixup;
   });
   qq = prev.qq.overrideAttrs (oldAttrs: {
-    postInstall = (oldAttrs.postInstall or "") + ''
-      wrapProgram $out/bin/qq --set GTK_IM_MODULE fcitx
-    '';
+    installPhase = builtins.replaceStrings [ "--add-flags" ] [ "--set GTK_IM_MODULE fcitx --add-flags" ] oldAttrs.installPhase;
   });
 }
